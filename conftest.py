5d2cc67e8321548cafcfa45ed1aa70a88b3db4f4
saravananatgithub <38750533+saravananatgithub@users.noreply.github.com>
2025-03-31 13:31:02 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,13 +3,4 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption("--a", action="store", type=int, help="First number")
     parser.addoption("--b", action="store", type=int, help="Second number")
-
-@pytest.fixture
-def input_values(request):
-    a = request.config.getoption("--a")
-    b = request.config.getoption("--b")
-
-    if a is None or b is None:
-        pytest.skip("Skipping test: Missing command-line arguments.")
-
-    return a, b
+    parser.addoption("--c", action="store", type=int, help="Third number")
