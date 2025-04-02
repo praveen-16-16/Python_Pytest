74915528bc33c1675aab54c6b8125c49e138df26
praveen-16-16 <rajp18733@gmail.com>
2025-04-02 20:08:13 +0530

    Update test_div.py

--- test_div.py
+++ test_div.py
@@ -5,6 +5,6 @@ def test_div(request):
     a = request.config.getoption("--a")
     b = request.config.getoption("--b")
     result = div(a, b)
-    expected = a + b
+    expected = a / b
     print(f"\nDivition ({a}, {b}) = {result}")
     assert result == expected, f"Expected {expected}, but got {result}"
