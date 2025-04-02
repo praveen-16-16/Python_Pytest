763f3156fe464e41bd8ed5333c7dd383c169d50c
praveen-16-16 <rajp18733@gmail.com>
2025-04-02 19:59:15 +0530

    Update test_div.py

--- test_div.py
+++ test_div.py
@@ -1,15 +1,10 @@
 import pytest
 from arithmetics import div
 
-def test_div(input_values):
-    a, b = input_values
-
-    if b == 0:
-        with pytest.raises(ValueError, match="Cannot divide by zero"):
-            div(a, b)
-        return  # Skip assertion if division by zero
-
+def test_div(request):
+    a = request.config.getoption("--a")
+    b = request.config.getoption("--b")
     result = div(a, b)
-    expected = a / b
-    print(f"\nDivision ({a}, {b}) = {result}")
+    expected = a + b
+    print(f"\nDivition ({a}, {b}) = {result}")
     assert result == expected, f"Expected {expected}, but got {result}"
